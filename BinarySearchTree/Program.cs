dbb586ecc1c6d3918caf8b2268c7908ffa20a016
JR Mishra <jrmnitdgp@gmail.com>
2020-10-23 23:48:51 +0530

    [JR Mishra]Add. Created BST and ability to add and display

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree");
+
+            BstBuilder<int> bst = new BstBuilder<int>();
+            bst.AddData(10);
+            bst.AddData(12);
+            bst.AddData(4);
+            bst.AddData(7);
+            bst.AddData(22);
+
+            bst.Display(bst.rootNode);
+        }
+    }
+}
